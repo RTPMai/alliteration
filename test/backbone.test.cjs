2f049045edca3dc8f9e0d5217bd71d5a25a8efde
RTPMai <ryan@pmapparel.com>
2026-07-24 08:19:13 -0500

    Add backbone.test.cjs configuration file

---
+++ test/backbone.test.cjs
@@ -0,0 +1,16 @@
+{
+  "name": "alliteration",
+  "version": "0.1.0",
+  "private": true,
+  "type": "module",
+  "description": "Single shell hosting P&M Apparel's internal apps under one login.",
+  "engines": {
+    "node": "20.x"
+  },
+  "scripts": {
+    "test": "bash test/run.sh"
+  },
+  "dependencies": {
+    "@vercel/blob": "^2.6.1"
+  }
+}
